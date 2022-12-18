84188b42596f82fe7f65dbbdf31e5f55acd1c01a
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-12-18 10:45:22 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,41 +1,60 @@
  {
- "contactOwner": "Nomor ke 1 lu",
- "contactOwner2": "Nomor ke 2 lu", 
- "botName": "Nama Bot",
- "ownerName": "Nama lu",
+ "contactOwner": "6283849469400",
+ "contactOwner2": "6283802603905", 
+ "botName": "sky dark bot",
+ "ownerName": "kamu nanya nama aku?",
  "sessionName": "session",
- "footer": "Nama Bot © 2022",
- "packname": "Seterah",
- "author": "Seterah",
+ "footer": "sky dark bot © 2022",
+ "packname": "di buat untuk tidak di colong",
+ "author": "sky dark bot",
  
- "youtube": "YouTube lu kalo ga punya kasih tanda min (-) ajah",
- "github": "Github lu",
+ "youtube": "(-)",
+ "github": "https://github.com/Doramisi1",
  "instagram": "Instagram lu",
  "tiktok": "Tiktok lu",
  "website": "Website lu",
- "email": "Email lu",
- "gender": "Gender/Alat kelamin lu",
- "agama": "Agama lu",
- "tanggallahir": "Tanggal lahir lu",
- "umur": "Umur lu",
- "kelas": "Kelas lu",
- "hobi": "Hobi lu",
- "sifat": "Sifat lu",
- "tinggal": "Tempat tinggal lu",
+ "email": "egabertoananta65@gmail.com",
+ "gender": "cowok",
+ "agama": "kepo anda",
+ "tanggallahir": "18-02-tahun nya cari aja sendiri",
+ "umur": "hitung sendiri",
+ "kelas": "kepo anda",
+ "hobi": "ngoding/gamer/nyanyi/dll",
+ "sifat": "hangat kalau lagi mood",
+ "tinggal": "Indonesia, sumut, medan",
  "suka": "Lu suka apa? ",
  "benci": "Lu benci apa? ",
  
-  "login_di": "https://api.zeeoneofc.xyz",
-  "apikey_alpha": "APIKEY LU", 
-  
-  "login_di": "https://ronzxapis.my.id",
-  "apikey_ronzz": "APIKEY LU",
- 
- "login_di": "https://zenzapis.xyz",
- "api_zenz": "APIKEY LU", 
- 
- "login_di": "https://atlantic-pedia.co.id/reff/RonzzOfc",
- "apikey_antlatic": "APIKEY LU", 
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  amel: 'https://melcanz.com',
+  males: 'https://malesin.xyz',
+  hardianto: 'https://hardianto.xyz',
+  lol: 'https://api.lolhuman.xyz',
+  adicug: 'https://api.adiofficial.xyz'
+}
+global.lolkey = 'a243d5d9b0d811b81d63e6cf'
+global.xkey = 'YourApikey'
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://api.xteam.xyz': '6aceac403a3d479b',
+  'https://melcanz.com': 'apikeylu',
+  'https://api.lolhuman.xyz': 'apikeylu',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apikeylu',
+  'https://pencarikode.xyz': 'apikeylu',
+  'https://hardianto.xyz': 'hardianto',
+  'https://leyscoders-api.herokuapp.com': 'apikeylu',
+  'https://api.adiofficial.xyz': 'apikey lu'
  
  "payment": {
     "qris": {
@@ -43,8 +62,8 @@
       "atas_nama": "Nama lu" 
     },
     "dana": {
-      "nomer": "Nomor dana lu", 
-      "atas_nama": "Nama lu" 
+      "nomer": "6283849469400", 
+      "atas_nama": "ega" 
     }},
     
   "pathQris": "./temp/media/qris.jpg", 
